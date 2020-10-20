8fcecc505e453fe3e9955a5a259fea34214362e8
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-10-20 14:12:00 +0530

    post  endpoints

--- cps-sma-be/src/routes/posts.js
+++ cps-sma-be/src/routes/posts.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.send('hello from posts');
 });
 
+/*------------------------------------------ Add Post ---------------------------*/
+router.post('/addPost', function(req, res, next) {
+  res.send('hello from posts');
+});
+
+/*------------------------------------------ Get Posts For User ---------------------------*/
+router.get('/getPosts/:username', function(req, res, next) {
+  res.send('hello from posts');
+});
+
+router.put('/editPost/:postId', function(req, res, next) {
+  res.send('hello from posts');
+});
+
 module.exports = router;
