070a05815edb5fa543ab8351b82535cb4f0f111f
umesh-lewa <48169702+umesh-lewa@users.noreply.github.com>
2020-10-20 22:35:11 +0530

    add useremail field  to post  schema

--- cps-sma-be/src/models/Post.js
+++ cps-sma-be/src/models/Post.js
@@ -1,26 +1,27 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new Schema({
-    body: String,
-    username: String,
-    createdAt: String,
-    comments: [
-        {
-            body: String,
-            username: String,
-            createdAt: String
-        }
-    ],
-    likes: [
-        {
-            username: String,
-            createdAt: String
-        }
-    ],
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
+const PostSchema = new mongoose.Schema({
+  body: String,
+  username: String,
+  useremail: String,
+  createdAt: String,
+  comments: [
+    {
+      body: String,
+      username: String,
+      createdAt: String
     }
+  ],
+  likes: [
+    {
+      username: String,
+      createdAt: String
+    }
+  ],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  }
 });
 
 module.exports = mongoose.model("Post", PostSchema);
